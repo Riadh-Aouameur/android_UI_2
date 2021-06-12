ac8f3a6102ffb5048e65891f39c83a0c7e9f1e82
Riadh-Aouameur <riadhaouameur02@gmail.com>
2021-06-12 19:40:24 +0100

    first commit

---
+++ app/src/main/java/com/example/restaurant/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+
+        setContentView(R.layout.activity_main);
+    }
+}
